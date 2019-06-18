c08a571add46c6e7576600e2e8057547602435ab
Saulo Alexandre de Barros <sab2@ug3c34.windows.cin.ufpe.br>
2019-06-18 11:28:45 -0300

    Resolucao prova 2019.1

---
+++ Provas/2019.1/ex4.lua
@@ -0,0 +1,24 @@
+function newpoly(lista, x)
+
+    final = 0
+    arraynumeros = {}
+
+    for i,v in pairs(lista) do
+        arraynumeros[i] = v
+    end
+    
+    final = arraynumeros[1] * x * x +  arraynumeros[2] * x + arraynumeros[3] 
+
+    return final
+
+end
+
+listaComOsPolinomios = {3,0,1}
+
+function f(x)
+    return newpoly(listaComOsPolinomios, x)
+end
+
+print(f(0)) --> 1
+print(f(5)) --> 76
+print(f(10)) --> 301
